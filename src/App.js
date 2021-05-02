a74fd63ed5f02b86e9ad3c1d318730e6ce0d431a
Suhas Nidgundi <suhasnidgundi@gmail.com>
2021-05-02 15:15:42 +0530

    Version 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Reports from './pages/Reports';
+import Products from './pages/Products';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/reports' component={Reports} />
+          <Route path='/products' component={Products} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
